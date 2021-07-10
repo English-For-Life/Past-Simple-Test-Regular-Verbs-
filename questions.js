1f1afc2780f12ea114d08bb6068aba8496fc5dd2
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-07-10 12:48:10 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,82 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"My classmate and I … our teacher a question about the test.",
+        options:["asked", "ask", "was asking"],
+        answer: 0
+    },
+    {
+        q:"John wanted to … work yesterday but he was sick.",
+        options:["went", "go to", "go"],
+        answer: 1
+    },
+    {
+        q:"They … to the park because they were very tired.",
+        options:["no walk", "not walk", "didn’t walk"],
+        answer: 2
+    },
+    {
+        q:"Did you talk to your boss? - Yes, I …   .",
+        options:["did", "talked", "was"],
+        answer: 0
+    },
+    {
+        q:"He … twenty minutes for the bus yesterday.",
+        options:["was wait", "waited", "waits"],
+        answer: 1
+    },
+    {
+        q:" … they fix their bicycles? - Yes, they …   .",
+        options:["Did / fixed", "Were / did", "Did / did"],
+        answer: 2
+    },
+    {
+        q:"They … go camping in a park when they were children.",
+        options:["liked to", "liked", "didn't like"],
+        answer: 0
+    },
+    {
+        q:"I had to … dinner last night, so I couldn’t watch the TV program.",
+        options:["cooked", "cook", "cooking"],
+        answer: 1
+    },
+    {
+        q:"Did you … the book? - No, I …   .",
+        options:["finish / not finish", "finishing / wasn’t", "finish / didn’t"],
+        answer: 2
+    },
+    {
+        q:"Why … you wash the dirty dishes last week?",
+        options:["didn't", "weren't", "not"],
+        answer: 0
+    },
+    {
+        q:"He … see a dentist yesterday because he had a toothache.",
+        options:["wants to", "wanted to", "want"],
+        answer: 1
+    },
+    {
+        q:"I … at the library very late last night.",
+        options:["was stayed", "did stayed", "stayed"],
+        answer: 2
+    },
+    {
+        q:"Mr. Johnson … for help when he fell in the water.",
+        options:["shouted", "shouts", "was shout"],
+        answer: 0
+    },
+    {
+        q:"She didn’t answer the phone because she … hear it ring.",
+        options:["weren't", "didn't", "wasn't"],
+        answer: 1
+    },
+    {
+        q:"I ... tennis yesterday because I don’t know how to play tennis.",
+        options:["not played", "didn’t played", "didn’t play"],
+        answer: 2
+    }
+
+
+
+]
